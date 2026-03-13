a5f641f6a8fc6d2836a776bc461f35b37882d4b6
venkatesh <yellinkalav@gmail.com>
2026-03-13 19:35:23 +0530

    fix static files

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+import os
+
+# Base directory
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Static files
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
